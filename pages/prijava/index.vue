87310c0e4d1cceef1d282fe8ced96610053f69ff
Marko Tomljanović <markot@windowslive.com>
2022-11-10 15:14:19 +0100

    feat: izgled forme prijave i regisracije

--- pages/prijava/index.vue
+++ pages/prijava/index.vue
@@ -1,25 +1,57 @@
 <template>
-  <b-container fluid="sm" class="mt-3" style="border-style: dotted">
-    <b-row
-      ><b-col md="6" class="mt-3 mx-auto"><h2>Prijava</h2></b-col></b-row
-    >
-    <b-row>
-      <b-col md="6" class="mx-auto"
-        ><b-form @submit="onSubmit" class="mt-2">
-          <b-form-input v-model="form.email" placeholder="email"></b-form-input>
-          <b-form-input
-            class="mt-2"
-            v-model="form.password"
-            placeholder="lozinka"
-          ></b-form-input>
+  <div class="container mt-5">
+    <div class="screen">
+      <div class="screen__content">
+        <b-form @submit="onSubmit" class="login">
+          <div class="login__field">
+            <i class="login__icon fas fa-user"></i>
+            <b-form-input
+              type="text"
+              class="login__input"
+              v-model="form.email"
+              placeholder="Email"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <b-form-input
+              type="password"
+              class="login__input"
+              v-model="form.password"
+              placeholder="Lozinka"
+            ></b-form-input>
+          </div>
 
-          <b-button class="mt-2" type="submit" variant="danger"
-            >Prijava</b-button
-          >
-        </b-form></b-col
-      ></b-row
-    >
-  </b-container>
+          <b-button class="button login__submit" type="submit">
+            <span class="button__text">Prijavi se sada</span>
+            <i class="button__icon fas fa-chevron-right"></i>
+          </b-button>
+        </b-form>
+        <div class="social-login">
+          <h3>prijava</h3>
+          <div class="social-icons">
+            <a href="#" class="social-login__icon fab fa-instagram"></a>
+            <a href="#" class="social-login__icon fab fa-facebook"></a>
+            <a href="#" class="social-login__icon fab fa-twitter"></a>
+          </div>
+        </div>
+      </div>
+      <div class="screen__background">
+        <span
+          class="screen__background__shape screen__background__shape4"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape3"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape2"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape1"
+        ></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -53,3 +85,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@import "@/assets/css/formaPrijava";
+
+.screen {
+  height: 600px;
+}
+.screen__background__shape1 {
+  height: 620px;
+}
+.login {
+  padding-top: 150px;
+}
+</style>
--- pages/registracija/index.vue
+++ pages/registracija/index.vue
@@ -1,41 +1,87 @@
 <template>
-  <b-form @submit="onSubmit" class="container mt-5">
-    <b-form-input
-      class="mb-2"
-      v-model="form.imePrezime"
-      placeholder="ime i prezime"
-    ></b-form-input>
-    <b-form-input
-      v-model="form.email"
-      placeholder="Enter your email"
-    ></b-form-input>
-    <b-form-input
-      class="mt-2"
-      v-model="form.password"
-      placeholder="Enter your password"
-    ></b-form-input>
-    <b-form-input
-      class="mt-2"
-      v-model="form.repeatPasswrod"
-      placeholder="Repeat your password"
-    ></b-form-input>
-    <b-form-input
-      class="mt-2"
-      v-model="form.mobitel"
-      placeholder="Mobitel"
-    ></b-form-input>
-    <b-form-input
-      class="mt-2"
-      v-model="form.mjesto"
-      placeholder="Mjesto"
-    ></b-form-input>
-
-    <b-button class="mt-2" type="submit" variant="danger">Registriraj</b-button>
-
-    <b-button to="/" class="mt-2" type="button" variant="danger"
-      >Naslovna</b-button
-    >
-  </b-form>
+  <div class="container mt-4">
+    <div class="screen">
+      <div class="screen__content">
+        <b-form @submit="onSubmit" class="login">
+          <div class="login__field">
+            <i class="login__icon fas fa-user"></i>
+            <b-form-input
+              type="text"
+              class="login__input"
+              v-model="form.imePrezime"
+              placeholder="Ime i prezime"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-user"></i>
+            <b-form-input
+              type="text"
+              class="login__input"
+              v-model="form.email"
+              placeholder="Email"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <b-form-input
+              type="password"
+              class="login__input"
+              v-model="form.password"
+              placeholder="Lozinka"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <b-form-input
+              type="password"
+              class="login__input"
+              v-model="form.repeatPasswrod"
+              placeholder="Ponovljena lozinka"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <b-form-input
+              type="tel"
+              class="login__input"
+              v-model="form.mobitel"
+              placeholder="Mobitel"
+            ></b-form-input>
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <b-form-input
+              type="text"
+              class="login__input"
+              v-model="form.mjesto"
+              placeholder="Mjesto"
+            ></b-form-input>
+          </div>
+          <b-button class="button login__submit" type="submit">
+            <span class="button__text">Registriraj se sada</span>
+            <i class="button__icon fas fa-chevron-right"></i>
+          </b-button>
+        </b-form>
+        <div class="social-login">
+          <h3>registracija</h3>
+        </div>
+      </div>
+      <div class="screen__background">
+        <span
+          class="screen__background__shape screen__background__shape4"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape3"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape2"
+        ></span>
+        <span
+          class="screen__background__shape screen__background__shape1"
+        ></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -102,3 +148,7 @@ export default {
   mounted() {},
 };
 </script>
+
+<style scoped>
+@import "@/assets/css/formaPrijava";
+</style>
