fe880b1c04d487211abc45953b3e0e0653fd6e41
Marko Tomljanović <markot@windowslive.com>
2022-12-01 11:26:58 +0100

    fix: test redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,19 @@
 export default async function ({ route, redirect, store, app }) {
-if((store.state.userData && !store.state.userDataF)){
-   try {
-  const us =await app.$fire.firestore
-    .collection("users")
-    .doc(store.state.userData.uid)
-    .get()
-  store.commit("setUserDataF", us.data());
-} catch (e) {
-  console.log(e);
-}}
- 
- 
+  if (store.state.userData && !store.state.userDataF) {
+    try {
+      const us = await app.$fire.firestore
+        .collection("users")
+        .doc(store.state.userData.uid)
+        .get();
+      store.commit("setUserDataF", us.data());
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  console.log(route.path);
   if (
     (!store.state.userData && route.path === "/novaknjiga") ||
-    (!store.state.userData && route.path.includes('profil'))
+    (!store.state.userData && route.path.includes("profil"))
   ) {
     return redirect("/prijava");
   } else if (
@@ -21,11 +21,13 @@ if((store.state.userData && !store.state.userDataF)){
     (store.state.userData && route.path === "/registracija")
   ) {
     return redirect("/");
-  }
-  else if((store.state.userData && route.params?.idProdavaca === store.state.userData.uid)){
+  } else if (
+    store.state.userData &&
+    route.params?.idProdavaca === store.state.userData.uid
+  ) {
     return redirect(`/profil/${store.state.userData.uid}`);
   }
-//  else if(store.state.podKategorija){alert("uspio sam")}
+  //  else if(store.state.podKategorija){alert("uspio sam")}
 }
 
 // export default function ({ route, redirect, store }) {
